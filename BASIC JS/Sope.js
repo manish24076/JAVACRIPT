d0e01847fb5ab3e6be505dba6f75cbf805e03d9d
manish24076 <manish24076@gmail.com>
2025-03-12 12:33:52 +0530

    JS SERIES

---
+++ BASIC JS/Sope.js
@@ -0,0 +1,34 @@
+/*let a =10;
+//var b=23;
+const c=22;
+
+console.log(a);
+console.log(b);
+console.log(c);*/
+
+//why we use let ,var, const in js //
+
+/*if(true) {
+let a =10;
+var b=23;
+const c=22;
+
+}
+
+/*console.log(a);
+console.log(c);
+console.log(b);*/
+
+
+// what is global scope ? 
+//OUTPUT [ inner 6    100]
+
+
+let a = 100;
+
+if(true){
+    let a = 66;
+    console.log("inner",a);
+}
+
+console.log(a);
