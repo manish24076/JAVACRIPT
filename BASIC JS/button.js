bb836004062e0a1376f8949ceb9833094a2b364d
manish kumar <117361711+manish24076@users.noreply.github.com>
2025-03-12 20:07:12 +0530

    Add files via upload

---
+++ BASIC JS/button.js
@@ -0,0 +1,20 @@
+//when we click button so change the background color//
+
+/*document.getElementById("a").onclick = function(){
+    document.body.style.backgroundColor ='blue'
+
+}
+
+document.getElementById("d").onclick = function(){
+    document.body.style.backgroundColor ='red'
+
+}  */
+
+    function clickhandler(){
+       document.getElementById("a").onclick= alert("hello");
+
+
+
+    }
+
+    document.getElementById("a").onclick = clickhandler
