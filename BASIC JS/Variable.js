357e6f6106b7b0c23d16f40d4a2abdf9ccb263bd
manish kumar <117361711+manish24076@users.noreply.github.com>
2025-03-16 20:55:24 +0530

    Add files via upload

---
+++ BASIC JS/Variable.js
@@ -0,0 +1,32 @@
+//-------Let , Var , Const----- ????
+
+//let accountemail = "codeing@gmail.com"
+var accountpass ="1234"
+//const accountid="535332"
+
+/*console.log(accountemail);
+console.log(accountpass);
+console.log(accountid);*/
+
+// But after Updation//
+let accountemail = "vscodeg@gmail.com"
+var accountpass ="346476"
+const accountid="976755"
+
+console.log(accountemail);
+console.log(accountpass);
+console.log(accountid); 
+
+//TABLE FORM//
+
+/*let accountemail = "codeing@gmail.com"
+var accountpass ="1234"
+const accountid="535332"*/
+
+console.table([accountemail,accountpass,accountid])
+
+
+
+
+
+
