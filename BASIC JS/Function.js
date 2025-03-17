0273f1d074fa3caa246c7ba67228d4139cd1a553
manish kumar <117361711+manish24076@users.noreply.github.com>
2025-03-17 11:47:52 +0530

    Add files via upload

---
+++ BASIC JS/Function.js
@@ -0,0 +1,18 @@
+function loginuser(username){
+   if (!username) {
+    console.log("enter username");
+    return    
+    
+   }
+   
+    return `${username} you login  `
+    
+
+
+}
+
+console.log(loginuser("sam"));
+console.log(loginuser(""));
+console.log(loginuser());
+
+  
