4796034fc54ed3a16bb1b6f6886e97590f54dea9
manish kumar <117361711+manish24076@users.noreply.github.com>
2025-03-18 21:23:58 +0530

    Update function2.js

--- BASIC JS/function2.js
+++ BASIC JS/function2.js
@@ -1,5 +1,21 @@
-function  calculatecartprize(...num1){
+/*function  calculatecartprize(...num1){
     return num1
 }
 
-console.log(calculatecartprize(12,22));
+console.log(calculatecartprize(12,22));*/
+
+
+const user = {
+    username:"money",
+    price:178723,
+    city:"jk"
+}
+
+function handleoject(user){
+    console.log(`hello ${user.username} and your price ${user.price} in this ${user.city}`);
+    
+      
+}
+
+handleoject(user)
+
