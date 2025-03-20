018c0d2b0a4880a4fb7e75d498ba36c566a8d5bb
manish kumar <117361711+manish24076@users.noreply.github.com>
2025-03-20 21:59:50 +0530

    Add files via upload

---
+++ BASIC JS/array.js
@@ -0,0 +1,19 @@
+//let fruits = ["Apple", "Banana", "Cherry"];
+//console.log(fruits); 
+
+let numbers = [1, 2, 3];
+
+
+//console.log(numbers);
+
+//console.log(numbers.push(4));
+//console.log(numbers.pop(1));
+
+console.log(numbers);
+
+  numbers.push(4);    // [1, 2, 3, 4]
+//numbers.pop(3);      // [1, 2, 3]
+//numbers.unshift(0); // [0, 1, 2, 3]
+//numbers.shift(3);    // [1, 2, 3]
+
+
