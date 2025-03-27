d06865a83dcf9adbaab5f6c31d0d60d17801a6af
manish kumar <117361711+manish24076@users.noreply.github.com>
2025-03-27 15:56:22 +0530

    Add files via upload

---
+++ BASIC JS/DatatypeCoversion.js
@@ -0,0 +1,35 @@
+
+
+//let number = 0;
+//let number = 1;
+//let number = "";
+//let number = "sanu";
+
+//let testnumber =Boolean(number)
+//console.log(testnumber);
+
+
+
+//only testing//
+/*if (number>=1) {
+    console.log("login success",testnumber);
+    
+    
+}else{
+    console.log("Login fail",testnumber);
+    
+}*/
+
+/*...convert into String ......*/
+
+let number =23;
+console.log(typeof number);
+
+let convertnumber=String(number)
+
+console.log(convertnumber);
+
+console.log(typeof convertnumber);
+
+
+
